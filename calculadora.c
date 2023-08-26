a3c598d99bc2f33a14a5348498804023c66c843d
jptomazz <jpt312005@gmail.com>
2023-08-25 21:07:39 -0300

    calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 
 int main(){
+    int valor1;
+    int valor2;
+    int resultado;
+    char operacao;
 
-   printf("ola");
+    printf("Qual a operacao?\n");
+    scanf("%c", &operacao);
+    printf("Qual o primeiro valor?\n");
+    scanf("%d", &valor1);
+    printf("Qual o segundo numero?\n");
+    scanf("%d", &valor2);
+    
+    if(operacao == '+'){
+        printf("Operacao de soma e o resultado e: %d\n", valor1 + valor2);
+    }else if(operacao == '-') {
+        printf("Operacao de subtracao e o resultado e: %d\n", valor1 - valor2);
+    }else if (operacao == '*'){
+        printf("Operacao de multiplicacao e o resultado e: %d\n", valor1 * valor2);
+    }else if (operacao == '/'){
+        printf("Operacao de divisao e o resultado e: %d\n", valor1 / valor2);
+    }else if(operacao == '%') {
+        printf("Operacao de divisao e o resultado e: %d\n", valor1 % valor2);
+    } else if(operacao == 'c' && valor1 > valor2){
+            printf("%d e maior que %d\n", valor1, valor2);
+    } else if(operacao == 'c' && valor1 < valor2){
+        printf("%d e menor que %d\n", valor1, valor2);
+    } else if(operacao == 'c' && valor1 == valor2){
+        printf("%d e igual a %d\n", valor1, valor2);
+    } else {
+        printf("OPERACAO INVALIDA!!!\n");
+    
+    }
 
 
-
-
+    // SOMA resultado = valor1 + valor2;
+    // SUBTRACAO resultado = valor1 - valor2;
+    // MULTIPLICACAO resultado = valor1 * valor2;
+    // DIVISAO resultado = valor1 / valor2;
+    // OP DE RESTO resultado = valor1 % valor2;
+    // INCREMENTO valor1++;
+    // DECREMENTO valor1--;
 
     return 0;
 }
