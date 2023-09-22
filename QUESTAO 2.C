197e56c677648bb27e75644569065927d2ac082e
jptomazz <jpt312005@gmail.com>
2023-09-22 20:43:33 -0300

    questao 2

---
+++ QUESTAO 2.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int numero;
+    int soma=0;
+
+    scanf("%d", &numero);
+
+    switch(numero%2){
+        case 0
+            printf("o numero par!\n");
+            break;
+        default:
+            printf("o numero impar!\n");
+    } while (numero !=0){
+
+        soma+= numero%10;
+        numero /=10;
+    } printf("A soma dos algarismos %d\n" ,soma);
+
+
+    return 0;
+}
+
+
