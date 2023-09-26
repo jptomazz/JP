0a0bc5ced6a630142edf316be5c47e7a4ca7ad71
jptomazz <jpt312005@gmail.com>
2023-09-26 20:37:42 -0300

    QUESTAO 4.C

--- QUESTAO 4.C
+++ QUESTAO 4.C
@@ -1,21 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    float salario, novoSalario;
-    char nivelExperiencia;
+#include <stdio.h>
 
-    scanf("%c",&nivelExperiencia);
-    scanf("%f",&salario);
+int main() {
+    char nivel;
+    double salario, aumento, salario_atualizado;
 
-    if(nivelExperiencia == 'a'){
-        novoSalario = salario + (salario*0.05);
-        printf("R$ %.2f\n", novoSalario);
-    } else if(nivelExperiencia == 'b'){
-        novoSalario = salario + (salario*0.07);
-        printf("R$ %.2f\n", novoSalario);
-    } else if(nivelExperiencia == 'c'){
-        novoSalario = salario + (salario*0.08);
-        printf("R$ %.2f\n", novoSalario);
+    printf("Digite o nivel de experiencia (a, b ou c): ");
+    scanf(" %c", &nivel); 
+    printf("Digite o salario atual: ");
+    scanf("%lf", &salario);
+
+    switch (nivel) {
+        case 'a':
+            aumento = 0.05;
+            break;
+        case 'b':
+            aumento = 0.07;
+            break;
+        case 'c':
+            aumento = 0.08;
+            break;
+        default:
+            printf("nivel de experiencia invalido.\n");
+            return 1;
     }
+
+    salario_atualizado = salario + (salario * aumento);
+
+    printf("O novo salario e: %.2lf\n", salario_atualizado);
+
     return 0;
 }
