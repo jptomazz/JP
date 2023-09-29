260c0d25b518040622b1650f9e5eac8998566023
jptomazz <jpt312005@gmail.com>
2023-09-28 21:55:34 -0300

    QUESTAO 3.C

--- QUESTAO 3.C
+++ QUESTAO 3.C
@@ -1,38 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int senha_cadastrada, senha_inserida, tentativas;
+    int senha_cadastrada, senha_inserida;
     
-    // Lê a senha cadastrada inicialmente
+    printf("Digite a senha cadastrada (um numero de 4 digitos): \n");
     scanf("%d", &senha_cadastrada);
     
-    // Inicializa o número de tentativas
-    tentativas = 0;
-    
-    // Entra em um loop para receber as tentativas do usuário
     while (1) {
+        printf("Digite a senha para validacao: \n");
         scanf("%d", &senha_inserida);
-        tentativas++;
         
         if (senha_inserida == senha_cadastrada) {
-            // Senha correta, exibe mensagem e encerra o programa
-            printf("senha cadastrada: %d\nsenha valida!\n", senha_cadastrada);
-            break;
+            printf("senha valida!\n");
+            break;  
         } else {
-            // Senha incorreta, exibe mensagem de senha inválida
-            printf("senha invalida!\n");
-            
-            // Se o número de tentativas exceder 3, encerra o programa
-            if (tentativas >= 3) {
-                printf("Numero de tentativas excedido. Tente novamente mais tarde.\n");
-                break;
-            }
+            printf("senha invalida\n");
         }
     }
     
     return 0;
 }
-
 
 
 
